6d42bf9b98e105c5a746ca96840ba6dcfd5e879c
sophierichards <richas3@rpi.edu>
2019-10-01 20:08:30 -0400

    beginning of fishing

---
+++ EGD2/Assets/Scripts/Fishing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fishing : MonoBehaviour
+{
+    public bool isFishing = false;
+    [SerializeField]
+    [Range(0,1)]
+    private float fishingChance = .05f;
+    [SerializeField]
+    private float timeBetweenCheck = .1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
